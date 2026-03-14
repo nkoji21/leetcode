0f7d5e23e1fa056fe5a9f050d2f81c27b584e26a
nkoji <133028205+nkoji21@users.noreply.github.com>
2026-03-15 04:17:33 +0900

    Time: 0 ms (100%), Space: 8 MB (88.12%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,22 @@
+static bool is_palindrome_by_half_reverse(int x) {
+    int reversedLowerHalf = 0;
+
+    while (x > reversedLowerHalf) {
+        reversedLowerHalf = reversedLowerHalf * 10 + (x % 10);
+        x /= 10;
+    }
+
+    // 桁数が偶数か奇数
+    return (x == reversedLowerHalf || x == reversedLowerHalf / 10);
+}
+
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+
+    // 10の倍数で0以外回文ではない
+    if (x != 0 && x % 10 == 0)
+        return false;
+
+    return (is_palindrome_by_half_reverse(x));
+}
