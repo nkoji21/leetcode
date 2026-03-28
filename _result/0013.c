bee3bd33ddf923b8d214a4fd39124184b67c84dc
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-03-28 12:52:05 +0900

    problem 0013

--- _result/0013.c
+++ _result/0013.c
@@ -1,36 +1,40 @@
-#define I 1
-#define V 5
-#define X 10
-#define L 50
-#define C 100
-#define D 500
-#define M 1000
+#include <stdio.h>
+#include <string.h>
 
-size_t convertValue(char c) {
-    switch (c) {
-        case 'I': return I;
-        case 'V': return V;
-        case 'X': return X;
-        case 'L': return L;
-        case 'C': return C;
-        case 'D': return D;
-        case 'M': return M;
-    }
+static const struct {
+  char symbol;
+  int value;
+} roman_map[] = {
+    {'I', 1},   {'V', 5},   {'X', 10},   {'L', 50},
+    {'C', 100}, {'D', 500}, {'M', 1000},
+};
 
-    return 0;
+static int roman_value(char c) {
+  for (size_t i = 0; i < sizeof(roman_map) / sizeof(roman_map[0]); i++) {
+    if (roman_map[i].symbol == c)
+      return (roman_map[i].value);
+  }
+
+  return (0);
 }
 
-int romanToInt(char* s) {
-    int total = 0;
-    size_t len = strlen(s);
+int romanToInt(char *s) {
+  int total = 0;
 
-    for (int i = 0; i < len; i++) {
-        if (i < len - 1 && convertValue(s[i]) < convertValue(s[i+1])) {
-            total -= convertValue(s[i]);
-        } else {
-            total += convertValue(s[i]);
-        }
-    }
+  size_t s_len = strlen(s);
+  for (size_t i = 0; i < s_len; i++) {
+    int cur = roman_value((s[i]));
+    int next = (i + 1 < s_len) ? roman_value(s[i + 1]) : 0;
 
-    return (total);
+    total += (cur < next) ? -cur : cur;
+  }
+
+  return (total);
+}
+
+int main(int ac, char **av) {
+  (void)ac;
+
+  printf("%s\n", av[1]);
+  return 0;
 }
