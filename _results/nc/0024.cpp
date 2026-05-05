31bf9a2064b5518d2f36ef4538449a9616a75bc9
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-05-05 14:57:39 +0900

    problem 0024

--- _results/nc/0024.cpp
+++ _results/nc/0024.cpp
@@ -2,37 +2,28 @@
 using namespace std;
 
 /**
- * 思いついたのは、1つのスタック構造で
- * 2重ループでそれぞれの値をスタックに回していく方法かな。大きいのが見つかったら、それまでをpopして
- * 何回popできるかを出力する。
- *
- * けど、これだと計算量が O(n^2) かかりそうなのでTLEになると思う。
- *
- * ふぇてがMonotonic Stackって言ってたのと、Topicにもあるから調べてみる
- * ---
- *
- * 単調スタックとは、
- * スタックの中身が常に単調増加または単調減少になってるものらしい
- * 71 72 73 73 73 とか。
- *
- * 「次に〜となる日を求めて」とか、そういう時に使えるっぽい
- * O(N^2)が思いついた時にO(N)にできる方法
+ * DPで解けそう
  */
 class Solution {
  public:
   vector<int> dailyTemperatures(vector<int>& temperatures) {
     size_t n = temperatures.size();
-
-    stack<int> st;
     vector<int> ans(n, 0);
 
-    for (size_t i = 0; i < n; i++) {
-      while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
-        ans[st.top()] = i - st.top();
-        st.pop();
+    for (int i = n - 2; i >= 0; i--) {
+      int j = i + 1;
+      while (j < n && temperatures[j] <= temperatures[i]) {
+        if (ans[j] == 0) {
+          j = n;
+          break;
+        }
+
+        j += ans[j];
       }
 
-      st.push(i);
+      if (j < n) {
+        ans[i] = j - i;
+      }
     }
 
     return ans;
@@ -44,7 +35,7 @@ int main() {
   vector<int> temps = {73, 74, 75, 71, 69, 72, 76, 73};
 
   vector<int> ans = solution.dailyTemperatures(temps);
-  for (int& n : ans) {
+  for (int n : ans) {
     cout << n << ' ';
   }
   cout << endl;
